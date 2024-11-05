c584e26e8b67f0ea27fe2b512244cad772645e99
uo294532 <UO294532@uniovi.es>
2024-11-05 09:09:31 +0100

    finished sew

--- js/semaforo.js
+++ js/semaforo.js
@@ -18,11 +18,44 @@ class Semáforo{
         }
         let button1 = document.createElement("button");
         button1.setAttribute("type","button");
-        button1.appendChild(document.createTextNode("Comenzar"));
+        button1.onclick=this.initSequence.bind(this);
+        button1.appendChild(document.createTextNode("Arranque"));
         let button2 = document.createElement("button");
         button2.setAttribute("type","button");
-        button2.appendChild(document.createTextNode("Parar"));
+        button2.appendChild(document.createTextNode("Reacción"));
+        button2.setAttribute("disabled","disabled");
+        button2.onclick= this.stopReaction.bind(this);
         main.appendChild(button1);
         main.appendChild(button2);
+    }
+    initSequence(){
+        let main = document.querySelector("main");
+        main.classList.add("load");
+        let button = document.querySelector("button");
+        button.setAttribute("disabled","disabled");
+        setTimeout(()=>{
+            this.unload_moment=new Date();
+            this.endSequence();
+        },(this.difficulty*100)+1500);
+    }
+    endSequence(){
+        let button2 = document.querySelector("button:nth-of-type(2)");
+        button2.removeAttribute("disabled");
+        let main = document.querySelector("main");
+        main.classList.add("unload");
+    }
+    stopReaction(){
+        this.click_moment = new Date();
+        let reaction_time = this.click_moment.getMilliseconds() - this.unload_moment.getMilliseconds();
+        let main = document.querySelector("main");
+        let paragraph = document.createElement("p");
+        paragraph.appendChild(document.createTextNode("Your reaction time was: "+reaction_time.toString()+" ms"));
+        main.appendChild(paragraph);
+        main.classList.remove("load");
+        main.classList.remove("unload");
+        let begin = document.querySelector("button");
+        let reaction = document.querySelector("button:nth-of-type(2)");
+        begin.removeAttribute("disabled");
+        reaction.setAttribute("disabled","disabled");
     }
 }
