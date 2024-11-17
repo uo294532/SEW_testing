3e75fb3d54684627db6dc579e3030412c7e8708e
uo294532 <UO294532@uniovi.es>
2024-11-17 14:56:13 +0100

    preparing usability tests

---
+++ js/noticias.js
@@ -0,0 +1,44 @@
+class Noticias{
+    constructor(){
+        if(window.File){
+            $("input:last()").on("change",()=>this.readInputFile($("input:last()").prop("files")));
+        }else
+            $("main").append("<p>Este navegador no tiene soporte para la API necesaria. Para ver las noticias, porfavor use otro navegador.</p>");
+        let createNoticia=(function () {
+            console.log(this);
+            let title=$("input[name='Titulo']").val();
+            let text=$("input[name='Texto']").val();
+            let author=$("input[name='Autor']").val();
+            this.createNoticia(title,text,author);
+        }).bind(this);
+        $("button").on("click",createNoticia);
+    }
+    createNoticia(title,text,author){
+        title=$("<h3></h3>").append(title);
+        text=$("<p></p>").append(text);
+        author=$("<p></p>").append("Escrito por: "+author);
+        let article = $("<article></article>").append(title,text,author);
+        $("main").append(article);
+    }
+    readInputFile(files){
+        let file = files[0];
+        let tipoTexto = /text.*/;
+        if (file.type.match(tipoTexto)) {
+            var lector = new FileReader();
+            lector.onload = function (evento) {
+                let lineas = lector.result.split('\n');
+                lineas.forEach(element => {
+                    let line = element.split("_");
+                    let title=$("<h3></h3>").append(line[0]);
+                    let text=$("<p></p>").append(line[1]);
+                    let author=$("<p></p>").append("Escrito por: "+line[2]);
+                    let article = $("<article></article>").append(title,text,author);
+                    $("main").append(article);
+                });
+            }      
+            lector.readAsText(file);
+        }else {
+            console.log("Error : ¡¡¡ Archivo no válido !!!");
+        }     
+    }
+}
---
+++ js/viajes.js
@@ -0,0 +1,93 @@
+class Viajes{
+    constructor(){
+        navigator.geolocation.getCurrentPosition(this.getPosicion.bind(this), this.errorFunction.bind(this));
+    }
+    getPosicion(posicion){
+        this.mensaje = "Se ha realizado correctamente la petición de geolocalización";
+        this.longitud         = posicion.coords.longitude; 
+        this.latitud          = posicion.coords.latitude;  
+        this.precision        = posicion.coords.accuracy;
+        this.altitud          = posicion.coords.altitude;
+        this.precisionAltitud = posicion.coords.altitudeAccuracy;
+        this.rumbo            = posicion.coords.heading;
+        this.velocidad        = posicion.coords.speed;
+        this.getMapaEstaticoGoogle();       
+    }
+    errorFunction(error){
+        switch(error.code) {
+        case error.PERMISSION_DENIED:
+            this.mensaje = "El usuario no permite la petición de geolocalización";
+            break;
+        case error.POSITION_UNAVAILABLE:
+            this.mensaje = "Información de geolocalización no disponible";
+            break;
+        case error.TIMEOUT:
+            this.mensaje = "La petición de geolocalización ha caducado";
+            break;
+        case error.UNKNOWN_ERROR:
+            this.mensaje = "Se ha producido un error desconocido";
+            break;
+        }
+        $("main").prepend("<p>"+this.mensaje+"</p>");
+    }
+    verTodo(){
+        let datos='<p>'+ this.mensaje + '</p>'; 
+        datos+='<p>Longitud: '+this.longitud +' grados</p>'; 
+        datos+='<p>Latitud: '+this.latitud +' grados</p>';
+        datos+='<p>Precisión de la longitud y latitud: '+ this.precision +' metros</p>';
+        datos+='<p>Altitud: '+ this.altitude +' metros</p>';
+        datos+='<p>Precisión de la altitud: '+ this.precisionAltitud +' metros</p>'; 
+        datos+='<p>Rumbo: '+ this.rumbo +' grados</p>'; 
+        datos+='<p>Velocidad: '+ this.velocidad +' metros/segundo</p>';
+        $("main").append(datos);
+    }
+    getMapaEstaticoGoogle(){
+        var ubicacion=$("main");
+        var url = "https://maps.googleapis.com/maps/api/staticmap?";
+        var centro = "center=" + this.latitud + "," + this.longitud;
+        var zoom ="&zoom=15";
+        let width = Math.round(ubicacion.width());
+        var tamaño= "&size="+width+"x"+width;
+        var marcador = "&markers=color:red%7Clabel:S%7C" + this.latitud + "," + this.longitud;
+        var sensor = "&sensor=false"; 
+        var apiKey="&key=AIzaSyB1Thy6jl0ZgBTk-DldSMQMAKbP0x-1AOg";
+        
+        this.imagenMapa = url + centro + zoom + tamaño + marcador + sensor + apiKey;
+        ubicacion.prepend("<img src='"+this.imagenMapa+"' alt='mapa estático google' />");
+    }
+    initMap(){  
+        var centro = {lat: 43.3672702, lng: -5.8502461};
+        var mapaGeoposicionado = new google.maps.Map(document.getElementsByTagName('div')[0],{
+            zoom: 8,
+            center:centro,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+        
+        var infoWindow = new google.maps.InfoWindow;
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+    
+                infoWindow.setPosition(pos);
+                infoWindow.setContent('Localización encontrada');
+                infoWindow.open(mapaGeoposicionado);
+                mapaGeoposicionado.setCenter(pos);
+            }, function() {
+                handleLocationError(true, infoWindow, mapaGeoposicionado.getCenter());
+            });
+        } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, mapaGeoposicionado.getCenter());
+        }
+    }
+    handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                                'Error: Ha fallado la geolocalización' :
+                                'Error: Su navegador no soporta geolocalización');
+        infoWindow.open(mapaGeoposicionado);
+    }
+}
